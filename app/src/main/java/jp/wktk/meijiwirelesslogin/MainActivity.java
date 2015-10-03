fc788822aeb78f121bd1169a8dc88b0ed0bfbdf1
makotsait <tatants39-android@yahoo.co.jp>
2015-10-03 21:59:22 +0900

    Implementation of onClick

--- app/src/main/java/jp/wktk/meijiwirelesslogin/MainActivity.java
+++ app/src/main/java/jp/wktk/meijiwirelesslogin/MainActivity.java
@@ -4,6 +4,8 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -33,5 +35,10 @@ public class MainActivity extends AppCompatActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    public void onClick(View v){
+        //ボタンが押された時
+        Toast.makeText(MainActivity.this, "ボタン", Toast.LENGTH_SHORT).show();
     }
 }
