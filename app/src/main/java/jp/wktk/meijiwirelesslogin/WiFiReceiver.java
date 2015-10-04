ca6a5e540b628b75ceeb8222801eb39421cc2bae
wktk <wktk30@gmail.com>
2015-10-05 05:24:32 +0900

    Detect when WiFi gets available

--- app/src/main/java/jp/wktk/meijiwirelesslogin/WiFiReceiver.java
+++ app/src/main/java/jp/wktk/meijiwirelesslogin/WiFiReceiver.java
@@ -3,6 +3,8 @@ package jp.wktk.meijiwirelesslogin;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
 import android.net.wifi.WifiInfo;
 import android.net.wifi.WifiManager;
 
@@ -10,8 +12,20 @@ public class WiFiReceiver extends BroadcastReceiver {
 
     @Override
     public void onReceive(Context context, Intent intent) {
+        // Return unless connected to the MIND Wireless
         WifiManager wifiManager = (WifiManager) context.getSystemService(context.WIFI_SERVICE);
         WifiInfo wifiInfo = wifiManager.getConnectionInfo();
+        String ssid = wifiInfo.getSSID();
+        if (!ssid.equals("MIND-wireless-ap-n") && !ssid.equals("MIND-wireless-ap-bg")) {
+            return;
+        }
+
+        // When connected to MIND and available?
+        ConnectivityManager cManager = (ConnectivityManager) context.getSystemService(Context.CONNECTIVITY_SERVICE);
+        NetworkInfo nInfo = cManager.getNetworkInfo(ConnectivityManager.TYPE_WIFI);
+        if (nInfo != null && nInfo.isAvailable()) {
+            // Do the stuff
+        }
     }
 
 }
