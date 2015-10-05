52ca93807d8c81ed4cf950020248b4add1f77fab
wktk <wktk30@gmail.com>
2015-10-05 10:41:39 +0900

    Remove unused import

--- app/src/main/java/jp/wktk/meijiwirelesslogin/MainActivity.java
+++ app/src/main/java/jp/wktk/meijiwirelesslogin/MainActivity.java
@@ -2,8 +2,6 @@ package jp.wktk.meijiwirelesslogin;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
 import android.view.View;
 import android.widget.Toast;
 
