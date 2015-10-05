70b55e4ba13a821e2f61d7cd179f167a478b69fa
makotsait <makotsait@gmail.com>
2015-10-05 22:57:37 +0900

    Fixed some problems on MainActivity.java

--- app/src/main/java/jp/wktk/meijiwirelesslogin/MainActivity.java
+++ app/src/main/java/jp/wktk/meijiwirelesslogin/MainActivity.java
@@ -3,12 +3,16 @@ package jp.wktk.meijiwirelesslogin;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.text.InputType;
+import android.util.Log;
 import android.view.View;
 import android.widget.CheckBox;
 import android.widget.EditText;
 import android.widget.Toast;
 
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.io.OutputStream;
 import java.io.OutputStreamWriter;
 import java.io.PrintWriter;
@@ -39,14 +43,31 @@ public class MainActivity extends AppCompatActivity {
         id = editId.getText().toString();
         pass = editPass.getText().toString();
         try{
-            OutputStream out = openFileOutput("id.txt",MODE_PRIVATE);
-            PrintWriter writer = new PrintWriter(new OutputStreamWriter(out,"UTF-8"));
-            writer.append(id);
-            writer.append(pass);
-            writer.close();
+            OutputStream os1 = openFileOutput("id.txt",MODE_PRIVATE);
+            PrintWriter idWriter = new PrintWriter(new OutputStreamWriter(os1,"UTF-8"));
+            idWriter.append(id);
+            idWriter.close();
+            OutputStream os2 = openFileOutput("pass.txt",MODE_PRIVATE);
+            PrintWriter passWriter = new PrintWriter(new OutputStreamWriter(os2,"UTF-8"));
+            passWriter.append(pass);
+            passWriter.close();
             Toast.makeText(MainActivity.this, "入力完了", Toast.LENGTH_SHORT).show();
         }catch(IOException e){
             e.printStackTrace();
         }
     }
 }
+
+/*
+id,password読み込み用
+ try{
+            InputStream is1 = openFileInput("id.txt");
+            BufferedReader idReader = new BufferedReader(new InputStreamReader(is1,"UTF-8"));
+            Log.e("id", idReader.readLine());
+            InputStream is2 = openFileInput("pass.txt");
+            BufferedReader passReader = new BufferedReader(new InputStreamReader(is2,"UTF-8"));
+            Log.e("pass", passReader.readLine());
+    }catch(IOException e){
+            e.printStackTrace();
+        }
+ */
