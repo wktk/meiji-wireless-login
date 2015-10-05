ef1661a9ecbd06e6df1147835036df2ea3fb99ef
wktk <wktk30@gmail.com>
2015-10-05 10:37:09 +0900

    Remove menu button on mainActivity.  Close #2

--- app/src/main/java/jp/wktk/meijiwirelesslogin/MainActivity.java
+++ app/src/main/java/jp/wktk/meijiwirelesslogin/MainActivity.java
@@ -15,28 +15,6 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
     }
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
-    }
-
     public void onClick(View v){
         //ボタンが押された時
         Toast.makeText(MainActivity.this, "ボタン", Toast.LENGTH_SHORT).show();
