e14544c6d816e93ce6171e8ff62787257471d4cf
makotsait <makotsait@gmail.com>
2015-10-10 02:02:27 +0900

    Add new MainActivity that includes a setting list

--- app/src/main/java/jp/wktk/meijiwirelesslogin/MainActivity.java
+++ app/src/main/java/jp/wktk/meijiwirelesslogin/MainActivity.java
@@ -12,7 +12,6 @@ public class MainActivity extends PreferenceActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        //setContentView(R.layout.activity_main);
         addPreferencesFromResource(R.xml.preferences);
     }
 }
